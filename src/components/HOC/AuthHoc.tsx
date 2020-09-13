a2aaa9345a958645395dd6a1261b24cf2017e011
Kohei Tasaka <soundasleep@keio.jp>
2020-09-13 15:11:07 +0900

    can login with google

--- src/components/HOC/AuthHoc.tsx
+++ src/components/HOC/AuthHoc.tsx
@@ -1,14 +1,52 @@
 import React from "react";
+import { auth } from '../../config/';
+import { useRouter } from 'next/router'
+import Layout from "../Layout";
+import firebase from "firebase";
 
-export const AuthHoc = ({ auth, children, checkAuth }) => {
+const AuthHoc = (props: { children: React.ReactNode; }) => {
+  const [isFetching, setIsFetching] = React.useState(false);
+  const [isSignedIn, setIsSignedIn] = React.useState(false);
+  
+  const router = useRouter();
+
+  const checkAuth = async () => {
+    setIsFetching(true);
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        setIsSignedIn(true);
+        setIsFetching(false);
+      } else {
+        setIsFetching(false);
+        // router.push('/');
+      }
+    });
+  }
+
   React.useEffect(() => {
     checkAuth();
   }, []);
-  return !auth.isInitialized || auth.isFetching ? (
-    <LoadingPage />
-  ) : auth.isInitialized && !auth.isSignedIn ? (
-    <AuthPage />
-  ) : (
-    <React.Fragment>{children}</React.Fragment>
+
+  return isFetching ? (
+    <div>
+      <h1>Sign in....</h1>
+    </div>
+  ) : !isSignedIn ? (
+    <Layout>
+      <div>
+      <button
+       onClick={() => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+      }}
+    >
+      Login
+    </button>
+      </div>
+    </Layout>
+    ) : (
+    <React.Fragment>{props.children}</React.Fragment>
   );
 };
+
+export default AuthHoc;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app'
+import AuthHoc from '../components/HOC/AuthHoc'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <AuthHoc>
+      <Component {...pageProps} />
+    </AuthHoc>
+  );
+}
+
+export default App
